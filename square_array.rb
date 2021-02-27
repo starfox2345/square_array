b0299fbbca7028c9066cf03462a234e3b5ea906b
yatsun <yatsunlam25@gmail.com>
2021-02-27 01:18:37 -0500

    commit tree dd126a58dfdf0ce150c8b6b0fa9868b424b92e2b

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,7 @@
 def square_array(array)
   # your code here
     new_numbers = []
-#    counter = 1
 
-    array.each do |numbers| 
-      new_numbers << numbers**2
- #     counter += 1
-    end
+    array.each{|numbers| new_numbers << numbers**2}
     array = new_numbers
 end
